6254aa83641ce3a05087f718d5780bc2feb588b8
ChuGer <i.chuger@gmail.com>
2011-07-19 17:58:41 +0300

    adding line chart

--- grails-app/controllers/businessfinance/ReportController.groovy
+++ grails-app/controllers/businessfinance/ReportController.groovy
@@ -15,11 +15,78 @@ class ReportController {
   def index = {}
 
   def lineChart = {
+//    def lineChart = [
+//            [type: 'date', name: 'Data', data: [[2008, 1, 1], [2008, 2, 1], [2008, 3, 1]]],
+//            [type: 'number', name: 'Hours per Day', data: [11, 34, 465]],
+//            [type: 'number', name: 'Sales', data: [345, 3, 465]]
+//    ];
+
+    //dateMap contains startDate and map of two values by type 0 - outcome sum, 1 - income sum
+    def dateMap = [:]
+    Operation.findAll().each { o ->
+      def key = o.type == 1 ? 'in' : 'out'
+      def titleKey = o.type == 1 ? 'inT' : 'outT'
+      def date = o.startDate
+      def sum = o.sum
+      def title =  o.name
+
+      if (dateMap.containsKey(date)) {
+        def map = dateMap.get(date)
+        if (map.containsKey(key)) {
+          def oldSum = map.get(key)
+          def oldTitle = map.get(titleKey)
+          map.put(key, oldSum + sum)
+          map.put(titleKey, oldTitle +' '+ title)
+        } else {
+          map.put(key, sum)
+          map.put(titleKey, title)
+        }
+        println map
+        dateMap.put(date, map)
+      } else {
+        dateMap.put(date, [(key): sum, (titleKey) : title])
+      }
+    }
+//
+//    Operation.findAllByType(1).each { o ->
+//      def sumList = [:]
+//      if (dateMap.containsKey(o.startDate)) {
+//        sumList = dateMap.get(o.startDate)
+//        sumList.put('in', o.sum)
+//      } else {
+//        // out: undefined or null or so
+//        sumList = [in: o.sum]
+//      }
+//      dateMap.put(o.startDate, sumList)
+//    }
+
+    // creating 3 lists for JSON storage
+    def dateList = []
+    def outcomeList = []
+    def outcomeTitleList = []
+    def incomeList = []
+    def incomeTitleList = []
+    dateMap.each { o ->
+      dateList.add(o.key)
+      outcomeList.add(o.value.get('out'))
+      outcomeTitleList.add(o.value.get('outT'))
+      incomeList.add(o.value.get('in'))
+      incomeTitleList.add(o.value.get('inT'))
+    }
+
     def lineChart = [
-            [type: 'date', name: 'Data', data: [[2008, 1, 1], [2008, 2, 1], [2008, 3, 1]]],
-            [type: 'number', name: 'Hours per Day', data: [11, 34, 465]],
-            [type: 'number', name: 'Sales', data: [345, 3, 465]]
+            [type: 'date', name: 'Operation Date', data: dateList],
+            [type: 'number', name: g.message(code: 'operation.type.outcome'), data: outcomeList],
+            [type: 'string', name: 'outcomeTitle', data: outcomeTitleList],
+            [type: 'number', name: g.message(code: 'operation.type.income'), data: incomeList],
+            [type: 'string', name: 'incomeTitle', data: incomeTitleList],
     ];
+//    def lineChart = [
+//      [type:'date', name: 'Operation Date', data:[new Date(2011,6,5), new Date(2011,7,5)]],
+//      [type:'number', name:'Расход', data:[1000.0, 2000.0]],
+//      [type:'number', name:'Доход', data:[3000.0, 2000.0]]
+//      ]
+    println lineChart
     render lineChart as JSON
   }
 
@@ -53,8 +120,8 @@ class ReportController {
     // clicked row in pieChart
     def typeId = params.id.toInteger()
     // catView - type of operation (0 - outcome, 1 - income)
-    if (session.catView ==null)
-    session.catView = typeId
+    if (session.catView == null)
+      session.catView = typeId
     def clickedCatId = session.cat.get(typeId)
     recreatePieChart(clickedCatId)
     render session.pieChart as JSON
