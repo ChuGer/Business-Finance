973a130f38f013d37669021535b0735a89299ad0
ChuGer <i.chuger@gmail.com>
2011-07-26 21:06:56 +0300

    adding settings and persisting

--- grails-app/controllers/businessfinance/SettingController.groovy
+++ grails-app/controllers/businessfinance/SettingController.groovy
@@ -1,6 +1,7 @@
 package businessfinance
 
 import domain.auth.SecUser
+import domain.Settings
 
 class SettingController {
   static navigation = [
@@ -16,6 +17,35 @@ class SettingController {
   def index = {
     userService.saveUserInfo(this.class.simpleName)
     SecUser user = springSecurityService.getCurrentUser();
-    [user : user]
+    if (user != null) {
+      [user: user, settingsInstance: user.settings]
+    }
+  }
+
+  def update = {
+    def settingsInstance = Settings.get(params.id)
+    if (settingsInstance) {
+      if (params.version) {
+        def version = params.version.toLong()
+        if (settingsInstance.version > version) {
+
+          settingsInstance.errors.rejectValue("version", "default.optimistic.locking.failure", [message(code: 'settings.label', default: 'Settings')] as Object[], "Another user has updated this Settings while you were editing")
+          render(view: "index", model: [settingsInstance: settingsInstance])
+          return
+        }
+      }
+      settingsInstance.properties = params
+      if (!settingsInstance.hasErrors() && settingsInstance.save(flush: true)) {
+        flash.message = "${message(code: 'default.updated.message', args: [message(code: 'settings.label', default: 'Settings'), settingsInstance.id])}"
+        redirect(action: "index")
+      }
+      else {
+        render(view: "index", model: [settingsInstance: settingsInstance])
+      }
+    }
+    else {
+      flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'settings.label', default: 'Settings'), params.id])}"
+      redirect(action: "index")
+    }
   }
 }
